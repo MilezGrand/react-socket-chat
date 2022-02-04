f2679e94959fcafd37445762563cbe25642d683c
Vladislav Yakovlev <40297782+MilezGrand@users.noreply.github.com>
2022-02-05 01:45:37 +0500

    login screen update

--- src/App.js
+++ src/App.js
@@ -1,11 +1,36 @@
-import React from 'react';
+import { useEffect, useReducer } from 'react';
 import JoinBlock from './components/JoinBlock';
 import socket from './socket';
+import reducer from './reducer';
 
 function App() {
+  const [state, dispatch] = useReducer(reducer, {
+    joined: false,
+    roomId: null,
+    userName: null,
+  });
+
+  const onLogin = (obj) => {
+    dispatch({
+      type: 'JOINED',
+      payload: obj,
+    });
+    socket.emit('ROOM:JOIN', obj);
+  };
+
+  
+
+  useEffect(() => {
+    socket.on('ROOM:JOINED', users => {
+      console.log('новый пользователь', users);
+    });
+  }, []);
+
+  window.socket = socket;
+  
   return (
     <div className="App">
-      <JoinBlock/>
+      {!state.joined && <JoinBlock onLogin={onLogin}/>}
     </div>
 
   );
--- src/components/JoinBlock.jsx
+++ src/components/JoinBlock.jsx
@@ -1,16 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
+import axios from 'axios';
 
-function JoinBlock() {
+
+function JoinBlock({onLogin}) {
+    const [roomId, setRoomId] = useState('');
+    const [userName, setUserName] = useState('');
+    const [isLoading, setLoading] = useState(true);
+
+    const onEnter = async () => {
+        if (!roomId || !userName) {
+           return  alert('Неверные данные');
+        }
+        const obj = {
+            roomId,
+            userName,
+        }
+        setLoading(true);
+        await axios.post('/rooms', obj);
+        onLogin(obj);
+    };
+
     return (
         <div className='auth'>
             <div>
-            <input placeholder='Комната'></input>
+                <input placeholder='Комната' value={roomId} onChange={e => setRoomId(e.target.value)}/>
             </div>
             <div>
-            <input placeholder='Ваше имя'></input>
+                <input placeholder='Ваше имя' value={userName} onChange={e => setUserName(e.target.value)}/>
             </div>
             <div>
-            <button>Войти</button>
+                <button onClick={onEnter}>
+                    {isLoading ? 'ВОЙТИ' : 'ВХОД...'}
+                </button>
             </div>
             
         </div>
